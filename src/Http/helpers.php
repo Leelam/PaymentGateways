9fedbe4ddc524bd08c01b374832831f90591191b
GKrishna M <gm@leelam.com>
2016-05-10 20:51:15 +0530

    small fixes

---
+++ src/Http/helpers.php
@@ -0,0 +1,5 @@
+<?php
+
+/*
+ * Helper function
+ * */
--- src/LeelamPaymentGatewayServiceProvider.php
+++ src/LeelamPaymentGatewayServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace Leelam\PaymentGateway;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\ServiceProvider;
+use \Illuminate\Support\Facades\Config;
+use \Illuminate\Support\ServiceProvider;
 
 class LeelamPaymentGatewayServiceProvider extends ServiceProvider {
 
@@ -39,6 +39,29 @@ class LeelamPaymentGatewayServiceProvider extends ServiceProvider {
 		]);
 
 		$this->loadViewsFrom(__DIR__ . '/views', 'leelamPaymentGatewayConfig');
+
+
+		// Configuring with main route file
+		// Loading package view files directly from vendor directory as
+		require __DIR__ . '/Http/routes.php';
+
+		// Publishing packages views to /views
+		/*$this->publishes([
+			__DIR__ . '/views' => base_path('resources/views/comments'),
+		]);*/
+		// Publishing config file to /config
+		$this->publishes([
+			__DIR__ . '/config' => config_path(),
+		]);
+		// Publishing Migration File
+		$this->publishes([
+			__DIR__ . '/migrations' => $this->app->databasePath() . '/migrations'
+		], 'migrations');
+		// Publishing seeds file
+		/*$this->publishes([
+			__DIR__ . '/seeds' => $this->app->databasePath() . '/seeds'
+		], 'seeds');*/
+	
 
 	}
 
