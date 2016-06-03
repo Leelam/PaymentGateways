7f9ee2bc6196cd0d7506b9785be50e9daca7b244
GKrishna M <gm@leelam.com>
2016-06-04 00:24:18 +0530

    Fixes and Features.
    1. Added readableTransactionId
    2. cleanTransactionId - Back to id which is passed by payment gateway

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -1,11 +1,33 @@
 <?php
 
-/*
- * Helper function
- * */
 
-function cashConfig($key)
-{
+    function cashConfig ( $key )
+    {
 
-    return Config::get('cashConfig.' . $key);
-}
+        return Config::get ( 'cashConfig.' . $key );
+    }
+
+    /**
+     *  Converting string to readable
+     *  89ccb3f661 to 89C-CB3-F661
+     * @param $transactionId
+     * @return string
+     */
+    function readableTransactionId ( $transactionId )
+    {
+        return strtoupper ( substr ( chunk_split ( $transactionId, 3, '-' ), 0, -3 ) . substr ( $transactionId, 9, 1 ) );
+
+    }
+
+    /**
+     *  Reversing the readable tromg
+     *  89C-CB3-F661 to 89ccb3f661
+     * @param $transactionId
+     * @return string
+     */
+    function cleanTransactionId ( $transactionId )
+    {
+
+        return strtolower ( str_replace ( "-", "", $transactionId ) );
+
+    }
